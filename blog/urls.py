3ec8e597c6bba970d1e35795247dbc6a73185032
Adrian Schaub <adrian_121@web.de>
2021-03-03 16:07:54 +0100

    EigenBlog Post Detail Seite und html Erweiterung

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('account/',views.account, name='account'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
